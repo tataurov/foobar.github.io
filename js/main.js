d6fdcd9c3c434c9b6bc8c5e91b4cee1831ac50ea
Alexey Tataurov <zealav@gmail.com>
2020-12-22 12:08:09 +0500

    remove contacts hover effect

--- js/main.js
+++ js/main.js
@@ -38,49 +38,8 @@ $(document).ready(function(e) {
     }
   });
 
-  $('.with-hover-text, .regular-link').click(function(e) {
-    e.stopPropagation();
-  });
-
-  /***************
-   * = Hover text *
-   * Hover text for the last slide
-   ***************/
-  $('.with-hover-text').hover(
-    function(e) {
-      $(this).css('overflow', 'visible');
-      $(this).find('.hover-text')
-        .show()
-        .css('opacity', 0)
-        .animate({
-            paddingTop: '25px',
-            opacity: 1
-          },
-          'fast',
-          'linear'
-        );
-    },
-    function(e) {
-      var obj = $(this);
-      $(this).find('.hover-text')
-        .animate({
-            paddingTop: '0',
-            opacity: 0
-          },
-          'fast',
-          'linear',
-          function() {
-            $(this).hide();
-            $(obj).css('overflow', 'hidden');
-          }
-        );
-    }
-  );
-
   var img_loaded = 0;
   var j_images = [];
-
-
 });
 
 /******************
